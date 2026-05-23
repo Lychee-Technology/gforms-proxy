e65f57938199a36880f8b335665b98a209d6c882
Ruoshi Li <iceboundrock@gmail.com>
2026-05-23 16:02:20 -0700

    feat: add static form definition registry Signed-off-by: Ruoshi Li <iceboundrock@gmail.com>

---
+++ src/forms/registry.ts
@@ -0,0 +1,11 @@
+import type { FormDefinition } from '../lib/types.js';
+
+// Add imports here as new forms are registered:
+// import form1 from './1FAIpQLSabc123.json' with { type: 'json' };
+
+const registry = new Map<string, FormDefinition>([
+  // Add entries here:
+  // ['1FAIpQLSabc123', form1 as FormDefinition],
+]);
+
+export default registry;
